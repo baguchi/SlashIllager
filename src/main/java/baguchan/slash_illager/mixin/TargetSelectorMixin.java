079cccf200b65e7af8895f3474867e32def2378a
baguchan <shutux500@gmail.com>
2024-05-31 08:04:36 +0900

    fixed slash is sometime wrong

--- src/main/java/baguchan/slash_illager/mixin/TargetSelectorMixin.java
+++ src/main/java/baguchan/slash_illager/mixin/TargetSelectorMixin.java
@@ -1,20 +1,42 @@
 package baguchan.slash_illager.mixin;
 
 import baguchan.slash_illager.util.MobAttackManager;
+import mods.flammpfeil.slashblade.entity.IShootable;
 import mods.flammpfeil.slashblade.util.TargetSelector;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.ai.targeting.TargetingConditions;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.List;
 
 @Mixin(value = TargetSelector.class, remap = false)
 public class TargetSelectorMixin {
+    @Unique
+    private static Entity slashIllager$cachedEntity;
     @Redirect(method = "getTargettableEntitiesWithinAABB(Lnet/minecraft/world/level/Level;Lnet/minecraft/world/entity/LivingEntity;)Ljava/util/List;", at = @At(value = "INVOKE", target = "Lmods/flammpfeil/slashblade/util/TargetSelector;getAreaAttackPredicate(D)Lnet/minecraft/world/entity/ai/targeting/TargetingConditions;"))
     private static TargetingConditions injectTarget(double reach, Level world, LivingEntity attacker) {
         if (attacker instanceof Mob) {
+            return MobAttackManager.getAreaAttackPredicate(reach);
+        }
+        return TargetSelector.getAreaAttackPredicate(reach);
+    }
+
+    @Inject(method = "getTargettableEntitiesWithinAABB(Lnet/minecraft/world/level/Level;DLnet/minecraft/world/entity/Entity;)Ljava/util/List;", at = @At(value = "INVOKE", target = "Lmods/flammpfeil/slashblade/util/TargetSelector;getAreaAttackPredicate(D)Lnet/minecraft/world/entity/ai/targeting/TargetingConditions;"))
+    private static <E extends Entity & IShootable> void injectTarget(Level world, double reach, E owner, CallbackInfoReturnable<List<Entity>> cir) {
+        slashIllager$cachedEntity = owner;
+    }
+
+    @Redirect(method = "getTargettableEntitiesWithinAABB(Lnet/minecraft/world/level/Level;DLnet/minecraft/world/entity/Entity;)Ljava/util/List;", at = @At(value = "INVOKE", target = "Lmods/flammpfeil/slashblade/util/TargetSelector;getAreaAttackPredicate(D)Lnet/minecraft/world/entity/ai/targeting/TargetingConditions;"))
+    private static <E extends Entity & IShootable> TargetingConditions injectTarget(double reach) {
+        if (slashIllager$cachedEntity instanceof Mob) {
             return MobAttackManager.getAreaAttackPredicate(reach);
         }
         return TargetSelector.getAreaAttackPredicate(reach);
